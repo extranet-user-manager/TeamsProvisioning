9b9f96cd8f700af56b87b05d361cf5400d4cb59a
Tom Abbott <tomabbott@users.noreply.github.com>
2023-07-18 15:03:53 -0400

    existing runbook contents from EUMSites as of July 18 2023 (dev15)

--- Runbooks/CreateSite-Customizations.ps1
+++ Runbooks/CreateSite-Customizations.ps1
@@ -136,6 +136,11 @@ function CreateSite-Customizations {
                 $committeeTemplate = Get-PnPListItem -List PublisherPageTemplates -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>Group Member Committee Page</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
                 $meetingTemplate = Get-PnPListItem -List PublisherPageTemplates -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>Committee Meeting Page</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
 
+                # Get the EUM values needed for webpart
+                $eumAdminURL = Get-PnPListItem -List SuiteConfig -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>AdminURL</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+                $eumPortalURL = Get-PnPListItem -List SuiteConfig -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>PortalURL</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+                $eumAPIApplicationIDURI = Get-PnPListItem -List SuiteConfig -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>EUM-API-Application-ID-URI</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+
                 # EUM page templates found
                 if ($committeeTemplate -ne $null -and $committeeTemplate.Id -ne $null -and $meetingTemplate -ne $null -and $meetingTemplate.Id -ne $null) {
                     $folderName = $groupName.Replace(" ", "-").ToLower()
@@ -162,7 +167,16 @@ function CreateSite-Customizations {
 
             # Apply the Home Page Canvas Content
             Write-Verbose -Verbose -Message "Applying EUM-Committee-Template home page"
-            $canvasContent = Get-PnPFile -Url "/sites/Landing/PnPTemplates/EUM-Committee-Template.html" -Connection $connLandingSite -AsString
+            $canvasContent = Get-PnPFile -Url "$($SiteCollectionRelativeURL)/PnPTemplates/EUM-Committee-Template.html" -Connection $connLandingSite -AsString
+            
+            $eumAdminURLEncoded = $eumAdminURL -replace "https://", "https&#58;//"
+            $eumPortalURLEncoded = $eumPortalURL -replace "https://", "https&#58;//"
+            $eumAPIApplicationIDURIEncoded = $eumAPIApplicationIDURI -replace "api://", "api&#58;//"
+
+            $canvasContent = $canvasContent -replace "~eumAdminURL~", $eumAdminURLEncoded
+            $canvasContent = $canvasContent -replace "~eumPortalURL~", $eumPortalURLEncoded
+            $canvasContent = $canvasContent -replace "~eumAPIApplicationIDURI~", $eumAPIApplicationIDURIEncoded
+
             $pageNameWithExtension = "Home.aspx"
             $page = Get-PnPListItem -List "SitePages" -Query "<View><Query><Where><Eq><FieldRef Name='FileLeafRef'/><Value Type='String'>$($pageNameWithExtension)</Value></Eq></Where></Query></View>" -Connection $connNewSite
             $setPage = Set-PnPListItem -List SitePages -Identity $page.Id -Values @{"CanvasContent1" = $canvasContent } -UpdateType SystemUpdate -Connection $connNewSite
@@ -184,11 +198,16 @@ function CreateSite-Customizations {
                 # Get the EUM page templates for the pproject member page
                 $projectTemplate = Get-PnPListItem -List PublisherPageTemplates -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>Group Member Project Page</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
 
+                # Get the EUM values needed for webpart
+                $eumAdminURL = Get-PnPListItem -List SuiteConfig -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>AdminURL</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+                $eumPortalURL = Get-PnPListItem -List SuiteConfig -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>PortalURL</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+                $eumAPIApplicationIDURI = Get-PnPListItem -List SuiteConfig -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>EUM-API-Application-ID-URI</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+
                 # EUM page templates found
                 if ($projectTemplate -ne $null -and $projectTemplate.Id -ne $null) {
                     $folderName = $groupName.Replace(" ", "-").ToLower()
                     # Convert the Azure AD Group into an EUM group
-                    Add-PnPFile -FileName "$($site.GroupId).json" -Folder "UserManagerGroups" -Stream $stream -Connection $connEUMConfigSite -Values @{Title = "$($site.GroupId)"; EUMDisplayName = $groupName; Approvals = $false; SharePointGroup = "$($groupName) Visitors"; SharePointSiteCollectionURL = $siteURL; MemberPageEnabled = $true; GroupWelcomeEmailEnabled = $false; MemberPageTemplate = $committeeTemplate.Id; MemberPage = "Auto-generated URL"; MemberPageURL = "/members/$($folderName)" }
+                    Add-PnPFile -FileName "$($site.GroupId).json" -Folder "UserManagerGroups" -Stream $stream -Connection $connEUMConfigSite -Values @{Title = "$($site.GroupId)"; EUMDisplayName = $groupName; Approvals = $false; SharePointGroup = "$($groupName) Visitors"; SharePointSiteCollectionURL = $siteURL; MemberPageEnabled = $true; GroupWelcomeEmailEnabled = $false; MemberPageTemplate = $projectTemplate.Id; MemberPage = "Auto-generated URL"; MemberPageURL = "/members/$($folderName)" }
                 }
                 # No EUM page templates
                 else {
@@ -203,10 +222,142 @@ function CreateSite-Customizations {
 
             # Apply the Home Page Canvas Content
             Write-Verbose -Verbose -Message "Applying EUM-Project-Template home page"
-            $canvasContent = Get-PnPFile -Url "/sites/Landing/PnPTemplates/EUM-Project-Template.html" -Connection $connLandingSite -AsString
+            $canvasContent = Get-PnPFile -Url "$($SiteCollectionRelativeURL)/PnPTemplates/EUM-Project-Template.html" -Connection $connLandingSite -AsString
+            
+            $eumAdminURLEncoded = $eumAdminURL -replace "https://", "https&#58;//"
+            $eumPortalURLEncoded = $eumPortalURL -replace "https://", "https&#58;//"
+            $eumAPIApplicationIDURIEncoded = $eumAPIApplicationIDURI -replace "api://", "api&#58;//"
+            
+            $canvasContent = $canvasContent -replace "~eumAdminURL~", $eumAdminURLEncoded
+            $canvasContent = $canvasContent -replace "~eumPortalURL~", $eumPortalURLEncoded
+            $canvasContent = $canvasContent -replace "~eumAPIApplicationIDURI~", $eumAPIApplicationIDURIEncoded
+            
             $pageNameWithExtension = "Home.aspx"
             $page = Get-PnPListItem -List "SitePages" -Query "<View><Query><Where><Eq><FieldRef Name='FileLeafRef'/><Value Type='String'>$($pageNameWithExtension)</Value></Eq></Where></Query></View>" -Connection $connNewSite
             $setPage = Set-PnPListItem -List SitePages -Identity $page.Id -Values @{"CanvasContent1" = $canvasContent } -UpdateType SystemUpdate -Connection $connNewSite            
+        }
+        elseif (($pnpSiteTemplate -like "*EUM-Data-Room-Template.xml")) {
+            # Connect to the newly created site and get its group ID
+            $connNewSite = Helper-Connect-PnPOnline -Url $siteURL
+            $site = Get-PnPSite -Connection $connNewSite -Includes GroupId
+
+            # Make sure we have access to the Azure Automation variables
+            if (![string]::IsNullOrWhiteSpace($site.GroupId) -and $AzureAutomation) {
+                # Get and connect to the EUM Config site
+                $eumConfigSite = Get-AutomationVariable -Name 'EUMConfigSiteURL'
+                $connEUMConfigSite = Helper-Connect-PnPOnline -Url $eumConfigSite
+
+                # Get the EUM page templates for the pproject member page
+                $dataRoomTemplate = Get-PnPListItem -List PublisherPageTemplates -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>Group Member Document Sharing</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+
+                # Get the EUM values needed for webpart
+                $eumAdminURL = Get-PnPListItem -List SuiteConfig -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>AdminURL</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+                $eumPortalURL = Get-PnPListItem -List SuiteConfig -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>PortalURL</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+                $eumAPIApplicationIDURI = Get-PnPListItem -List SuiteConfig -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>EUM-API-Application-ID-URI</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+
+                #Copy Templates folder and contents to new site with metadata
+                Add-PnPFolder -Name "Templates" -Folder "Shared Documents" -Connection $connNewSite
+                Copy-PnPFile -SourceUrl "$($SiteCollectionRelativeURL)/PnPTemplates/Document Tracking.json" -TargetUrl "Shared Documents/Templates" -IgnoreVersionHistory -Connection $connNewSite -Force
+                #Wait 10 seconds for copy to complete
+                Start-Sleep -Seconds 10
+                $DocumentJson = Get-PnPFile -Url "Shared Documents/Templates/Document Tracking.json" -AsListItem -Connection $connNewSite
+                Set-PnPListItem -List "Documents" -Identity $DocumentJson.Id -Values @{"InternalDocumentType" = "Document Tracking JSON"} -Connection $connNewSite          
+                
+                Copy-PnPFile -SourceUrl "$($SiteCollectionRelativeURL)/PnPTemplates/Document Tracking.xlsx" -TargetUrl "Shared Documents/Templates" -IgnoreVersionHistory -Connection $connNewSite -Force
+                #Wait 10 seconds for copy to complete            
+                Start-Sleep -Seconds 10
+                $DocumentExcel = Get-PnPFile -Url "Shared Documents/Templates/Document Tracking.xlsx" -AsListItem -Connection $connNewSite  
+                Set-PnPListItem -List "Documents" -Identity $DocumentExcel.Id -Values @{"InternalDocumentType" = "Document Tracking Excel"} -Connection $connNewSite          
+                
+                # Create new permission level Contribute No Delete
+                Add-PnPRoleDefinition -RoleName "Contribute No Delete" -Clone "Contribute" -Exclude DeleteListItems -Connection $connNewSite
+}
+
+            # Apply the Home Page Canvas Content
+            Write-Verbose -Verbose -Message "Applying EUM-Data-Room-Template home page"
+            $canvasContent = Get-PnPFile -Url "$($SiteCollectionRelativeURL)/PnPTemplates/EUM-Data-Room-Template.html" -Connection $connLandingSite -AsString
+            
+            $eumAdminURLEncoded = $eumAdminURL -replace "https://", "https&#58;//"
+            $eumPortalURLEncoded = $eumPortalURL -replace "https://", "https&#58;//"
+            $eumAPIApplicationIDURIEncoded = $eumAPIApplicationIDURI -replace "api://", "api&#58;//"
+            $eumDataRoomURLEncoded = $siteURL -replace "https://", "https&#58;//"
+            
+            $canvasContent = $canvasContent -replace "~eumAdminURL~", $eumAdminURLEncoded
+            $canvasContent = $canvasContent -replace "~eumPortalURL~", $eumPortalURLEncoded
+            $canvasContent = $canvasContent -replace "~eumAPIApplicationIDURI~", $eumAPIApplicationIDURIEncoded
+            $canvasContent = $canvasContent -replace "~eumDataRoomURL~", $eumDataRoomURLEncoded
+            
+            $pageNameWithExtension = "Home.aspx"
+            $page = Get-PnPListItem -List "SitePages" -Query "<View><Query><Where><Eq><FieldRef Name='FileLeafRef'/><Value Type='String'>$($pageNameWithExtension)</Value></Eq></Where></Query></View>" -Connection $connNewSite
+            $setPage = Set-PnPListItem -List SitePages -Identity $page.Id -Values @{"CanvasContent1" = $canvasContent } -UpdateType SystemUpdate -Connection $connNewSite            
+        }
+        elseif (($pnpSiteTemplate -like "*template-partner.xml")) {
+            # Connect to the newly created site and get its group ID
+            $connNewSite = Helper-Connect-PnPOnline -Url $siteURL
+            $site = Get-PnPSite -Connection $connNewSite -Includes GroupId
+
+            # Make sure we have access to the Azure Automation variables
+            if (![string]::IsNullOrWhiteSpace($site.GroupId) -and $AzureAutomation) {
+                # Get and connect to the EUM Config site
+                $eumConfigSite = Get-AutomationVariable -Name 'EUMConfigSiteURL'
+                $connEUMConfigSite = Helper-Connect-PnPOnline -Url $eumConfigSite
+
+                #Build the empty JSON group file
+                $stream = [IO.MemoryStream]::new([Text.Encoding]::UTF8.GetBytes("{`"Members`": `"`"}"))
+                
+                # Get the EUM page templates for the partner member page
+                $partnerTemplate = Get-PnPListItem -List PublisherPageTemplates -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>Group Member Partner Page</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+
+                # Get the EUM values needed for webpart
+                $eumAdminURL = Get-PnPListItem -List SuiteConfig -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>AdminURL</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+                $eumPortalURL = Get-PnPListItem -List SuiteConfig -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>PortalURL</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+                $eumAPIApplicationIDURI = Get-PnPListItem -List SuiteConfig -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>EUM-API-Application-ID-URI</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+
+                # EUM page templates found
+                if ($partnerTemplate -ne $null -and $partnerTemplate.Id -ne $null) {
+                    $folderName = $groupName.Replace(" ", "-").ToLower()
+                    # Convert the Azure AD Group into an EUM group
+                    Add-PnPFile -FileName "$($site.GroupId).json" -Folder "UserManagerGroups" -Stream $stream -Connection $connEUMConfigSite -Values @{Title = "$($site.GroupId)"; EUMDisplayName = $groupName; Approvals = $false; SharePointGroup = "$($groupName) Visitors"; SharePointSiteCollectionURL = $siteURL; MemberPageEnabled = $true; GroupWelcomeEmailEnabled = $false; MemberPageTemplate = $partnerTemplate.Id; MemberPage = "Auto-generated URL"; MemberPageURL = "/members/$($folderName)" }
+                }
+                # No EUM page templates
+                else {
+                    Add-PnPFile -FileName "$($site.GroupId).json" -Folder "UserManagerGroups" -Stream $stream -Connection $connEUMConfigSite -Values @{Title = "$($site.GroupId)"; EUMDisplayName = $groupName; Approvals = $false; SharePointGroup = "$($groupName) Visitors"; SharePointSiteCollectionURL = $siteURL; MemberPageEnabled = $true; GroupWelcomeEmailEnabled = $false }
+                }
+            }   
+        }
+         elseif (($pnpSiteTemplate -like "*template-client.xml")) {
+            # Connect to the newly created site and get its group ID
+            $connNewSite = Helper-Connect-PnPOnline -Url $siteURL
+            $site = Get-PnPSite -Connection $connNewSite -Includes GroupId
+
+            # Make sure we have access to the Azure Automation variables
+            if (![string]::IsNullOrWhiteSpace($site.GroupId) -and $AzureAutomation) {
+                # Get and connect to the EUM Config site
+                $eumConfigSite = Get-AutomationVariable -Name 'EUMConfigSiteURL'
+                $connEUMConfigSite = Helper-Connect-PnPOnline -Url $eumConfigSite
+
+                #Build the empty JSON group file
+                $stream = [IO.MemoryStream]::new([Text.Encoding]::UTF8.GetBytes("{`"Members`": `"`"}"))
+                
+                # Get the EUM page templates for the partner member page
+                $clientTemplate = Get-PnPListItem -List PublisherPageTemplates -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>Group Member Client Page</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+
+                # Get the EUM values needed for webpart
+                $eumAdminURL = Get-PnPListItem -List SuiteConfig -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>AdminURL</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+                $eumPortalURL = Get-PnPListItem -List SuiteConfig -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>PortalURL</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+                $eumAPIApplicationIDURI = Get-PnPListItem -List SuiteConfig -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>EUM-API-Application-ID-URI</Value></Eq></Where></Query></View>" -Connection $connEUMConfigSite
+
+                # EUM page templates found
+                if ($clientTemplate -ne $null -and $clientTemplate.Id -ne $null) {
+                    $folderName = $groupName.Replace(" ", "-").ToLower()
+                    # Convert the Azure AD Group into an EUM group
+                    Add-PnPFile -FileName "$($site.GroupId).json" -Folder "UserManagerGroups" -Stream $stream -Connection $connEUMConfigSite -Values @{Title = "$($site.GroupId)"; EUMDisplayName = $groupName; Approvals = $false; SharePointGroup = "$($groupName) Visitors"; SharePointSiteCollectionURL = $siteURL; MemberPageEnabled = $true; GroupWelcomeEmailEnabled = $false; MemberPageTemplate = $clientTemplate.Id; MemberPage = "Auto-generated URL"; MemberPageURL = "/members/$($folderName)" }
+                }
+                # No EUM page templates
+                else {
+                    Add-PnPFile -FileName "$($site.GroupId).json" -Folder "UserManagerGroups" -Stream $stream -Connection $connEUMConfigSite -Values @{Title = "$($site.GroupId)"; EUMDisplayName = $groupName; Approvals = $false; SharePointGroup = "$($groupName) Visitors"; SharePointSiteCollectionURL = $siteURL; MemberPageEnabled = $true; GroupWelcomeEmailEnabled = $false }
+                }
+            }   
         }
         else {
             Write-Verbose "No customizations to apply"
